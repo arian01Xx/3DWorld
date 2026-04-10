813678532e3bda8008f9efe3b377c1929bffcedd
arian01Xx <arian_joseph@outlook.es>
2026-04-09 21:04:37 -0500

    editing

--- main.cpp
+++ main.cpp
@@ -44,9 +44,16 @@
 
 constexpr int TILE=5;
 
+struct Proj{
+    sf::Vector2f pos;
+    float scale;
+};
+
 struct World{
     int cell=180;
-    float angle=0.4f;
+
+    float angleX=0.3f;
+    float angleY=0.4f;
 
     std::vector<std::vector<int>> world={
         200, std::vector<int>(200,0)
@@ -57,51 +64,53 @@ struct World{
     int row=world.size();
     int col=world[0].size();
 
-    sf::Vector2f project(float x, float y, float z){
-        float d=27.0f; //estaba en 20.0f
-        float f=250.0f; //estaba en 200.0f
+    Proj project(float x, float y, float z){ 
+        float scale=5.0f;
 
-        z+=d;
-
-        float sx=(x/z)*f;
-        float sy=(y/z)*f;
+        float sx=x*scale;
+        float sy=y*scale;
 
         //centrar en pantalla
         sx+=middle*TILE;
         sy=(middle*TILE)-sy;
 
-        return {sx,sy};
+        return {{sx,sy}, 1.0f};
     }
 
     void drawAxisNumbers(sf::RenderWindow& window, sf::Font& font){
-        for(int i=-100; i<=100; i+=10){
+        float textScale=0.5f;
+
+        for(int i=-80; i<=80; i+=10){
 
             //EJE X
-            auto rotX=rotated(i,0,0,angle);
+            auto rotX=rotated(i,0,0);
             auto pX=project(rotX[0], rotX[1], rotX[2]);
             
             sf::Text textX(font, "", 12);
             textX.setFillColor(sf::Color::White);
             textX.setString(std::to_string(i));
-            textX.setPosition(pX);
+            textX.setPosition(pX.pos);
+            textX.setScale(sf::Vector2f(textScale*2, textScale*2));
 
             //EJE Y
-            auto rotY=rotated(0,i,0,angle);
+            auto rotY=rotated(0,i,0);
             auto pY=project(rotY[0], rotY[1], rotY[2]);
 
             sf::Text textY(font, "", 12);
             textY.setFillColor(sf::Color::White);
             textY.setString(std::to_string(i));
-            textY.setPosition(pY);
+            textY.setPosition(pY.pos);
+            textY.setScale(sf::Vector2f(textScale*2, textScale*2));
             
             //EJE Z
-            auto rotZ=rotated(0,0,i,angle);
+            auto rotZ=rotated(0,0,i);
             auto pZ=project(rotZ[0], rotZ[1], rotZ[2]);
 
             sf::Text textZ(font, "", 12);
             textZ.setFillColor(sf::Color::White);
             textZ.setString(std::to_string(i));
-            textZ.setPosition(pZ);
+            textZ.setPosition(pZ.pos);
+            textZ.setScale(sf::Vector2f(textScale*2, textScale*2));
 
             window.draw(textX);
             window.draw(textY);
@@ -109,15 +118,15 @@ struct World{
         }
     }
 
-    std::vector<float> rotated(float x, float y, float z, float angle){
+    std::vector<float> rotated(float x, float y, float z){
         //Y
-        float xr=x*std::cos(angle)+z*std::sin(angle);
+        float xr=x*std::cos(angleY)+z*std::sin(angleY);
         float yr=y;
-        float zr=-x*std::sin(angle)+z*std::cos(angle);
+        float zr=-x*std::sin(angleY)+z*std::cos(angleY);
 
         //X
-        float yr2=yr*std::cos(angle)-zr*std::sin(angle);
-        float zr2=yr*std::sin(angle)-zr*std::cos(angle);
+        float yr2=yr*std::cos(angleX)-zr*std::sin(angleX);
+        float zr2=yr*std::sin(angleX)+zr*std::cos(angleX);
 
         return {xr, yr2, zr2};
     } 
@@ -128,38 +137,38 @@ struct World{
 
         for(float t=-100; t<=100; t+=step){
             //EJE X
-            auto a=rotated(t,0,0,angle);
-            auto b=rotated(t+step,0,0,angle);
+            auto a=rotated(t,0,0);
+            auto b=rotated(t+step,0,0);
 
             auto p1=project(a[0], a[1], a[2]);
             auto p2=project(b[0], b[1], b[2]);
 
-            sf::Vertex v1; v1.position=p1; v1.color=sf::Color::Red;
-            sf::Vertex v2; v2.position=p2; v2.color=sf::Color::Red;
+            sf::Vertex v1; v1.position=p1.pos; v1.color=sf::Color::Red;
+            sf::Vertex v2; v2.position=p2.pos; v2.color=sf::Color::Red;
             line1.append(v1);
             line1.append(v2); 
 
             //EJE Y
-            auto aY=rotated(0,t,0,angle);
-            auto bY=rotated(0,t+step,0,angle);
+            auto aY=rotated(0,t,0);
+            auto bY=rotated(0,t+step,0);
 
             auto p1Y=project(aY[0], aY[1], aY[2]);
             auto p2Y=project(bY[0], bY[1], bY[2]);
 
-            sf::Vertex v1Y; v1Y.position=p1Y; v1Y.color=sf::Color::Green;
-            sf::Vertex v2Y; v2Y.position=p2Y; v2Y.color=sf::Color::Green;
+            sf::Vertex v1Y; v1Y.position=p1Y.pos; v1Y.color=sf::Color::Green;
+            sf::Vertex v2Y; v2Y.position=p2Y.pos; v2Y.color=sf::Color::Green;
             line2.append(v1Y);
             line2.append(v2Y);
 
             //EJE Z
-            auto aZ=rotated(0,0,t,angle);
-            auto bZ=rotated(0,0,t+step,angle);
+            auto aZ=rotated(0,0,t);
+            auto bZ=rotated(0,0,t+step);
 
             auto p1Z=project(aZ[0], aZ[1], aZ[2]);
             auto p2Z=project(bZ[0], bZ[1], bZ[2]);
 
-            sf::Vertex v1Z; v1Z.position=p1Z; v1Z.color=sf::Color::Cyan;
-            sf::Vertex v2Z; v2Z.position=p2Z; v2Z.color=sf::Color::Cyan;
+            sf::Vertex v1Z; v1Z.position=p1Z.pos; v1Z.color=sf::Color::Cyan;
+            sf::Vertex v2Z; v2Z.position=p2Z.pos; v2Z.color=sf::Color::Cyan;
             line3.append(v1Z);
             line3.append(v2Z);
         }
@@ -191,6 +200,9 @@ struct World{
 };
 
 void execute(){
+    bool dragging=false;
+    sf::Vector2i lastMouse;
+
     World w;
 
     sf::RenderWindow window{
@@ -208,6 +220,27 @@ void execute(){
     while(window.isOpen()){
         while(const std::optional event=window.pollEvent()){
             if(event->is<sf::Event::Closed>()) window.close();
+            if(event->is<sf::Event::MouseButtonPressed>()){
+                if(event->getIf<sf::Event::MouseButtonPressed>()->button==sf::Mouse::Button::Left){
+                    dragging=true;
+                    lastMouse=sf::Mouse::getPosition(window);
+                }
+            }
+            if(event->is<sf::Event::MouseButtonReleased>()){
+                if(event->getIf<sf::Event::MouseButtonReleased>()->button==sf::Mouse::Button::Left){
+                    dragging=false;
+                }
+            }
+        }
+
+        if(dragging){
+            sf::Vector2i current=sf::Mouse::getPosition(window);
+            sf::Vector2i delta=current-lastMouse;
+
+            w.angleY+=delta.x * 0.005f;
+            w.angleX+=delta.y * 0.005f;
+
+            lastMouse=current;
         }
 
         window.clear();
@@ -216,7 +249,7 @@ void execute(){
         w.drawAxisNumbers(window, font);
 
         window.display();
-    }
+    } 
 }
 
 int main(){
