905a9d93cf01f3ef3e0855f1a83527d8f2426c52
arian01Xx <arian_joseph@outlook.es>
2026-04-10 19:58:45 -0500

    working

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ struct Functions{
     //World w;
     //Functions(World& _w): w(_w) {}
 
-    void drawNode(float x, float y, float z, 
+    sf::Vector2f drawNode(float x, float y, float z, 
                   sf::RenderWindow& window, World& w){ 
         //solo pone un singular punto en el plano cartesiano
         sf::CircleShape Node(float(TILE-3));
@@ -24,27 +24,83 @@ struct Functions{
         Node.setPosition(Proj.pos);
 
         window.draw(Node);
+
+        return Proj.pos;
     }
 
-    void drawEdge(sf::RenderWindow, World& w){
-        sf::Vertex v;
-        //v.position();
+    void drawEdge(sf::Vector2f& PointX, sf::Vector2f& PointY, 
+                  sf::RenderWindow& window, World& w){
+        sf::VertexArray line(sf::PrimitiveType::Lines);
+        sf::Vertex Start; 
+        sf::Vertex End;
+        
+        Start.position=PointX;
+        Start.color=sf::Color::Red;
+
+        End.position=PointY;
+        End.color=sf::Color::Green;
+
+        line.append(Start);
+        line.append(End);
+
+        window.draw(line);
     }
 
-    void drawMotor(sf::RenderWindow& window, World& w){
+    void drawingBox(sf::RenderWindow& window, World& w){
         //DIBUJANDO UN CUADRADO:
         //PRIMERO DIBUJAR COORDENADAS DE CADA NODO
+        //TECHO SUPERIOR
         float x1=10.0f, y1=10.0f, z1=10.0f;
         float x2=10.0f, y2=10.0f, z2=-10.0f;
         float x3=-10.0f, y3=10.0f, z3=-10.0f;
         float x4=-10.0f, y4=10.0f, z4=10.0f;
-        drawNode(x1,y1,z1,window,w); //funciones escalables
-        drawNode(x2,y2,z2,window,w);
-        drawNode(x3,y3,z3,window,w);
-        drawNode(x4,y4,z4,window,w);
+        sf::Vector2f Point1=drawNode(x1,y1,z1,window,w); //funciones escalables
+        sf::Vector2f Point2=drawNode(x2,y2,z2,window,w);
+        sf::Vector2f Point3=drawNode(x3,y3,z3,window,w);
+        sf::Vector2f Point4=drawNode(x4,y4,z4,window,w);
+
+        //BASE INFERIOR
+        float x11=10.0f, y11=0.0f, z11=10.0f;
+        float x22=10.0f, y22=0.0f, z22=-10.0f;
+        float x33=-10.0f, y33=0.0f, z33=-10.0f;
+        float x44=-10.0f, y44=0.0f, z44=10.0f;
+
+        sf::Vector2f Point11=drawNode(x11,y11,z11,window,w);
+        sf::Vector2f Point22=drawNode(x22,y22,z22,window,w);
+        sf::Vector2f Point33=drawNode(x33,y33,z33,window,w);
+        sf::Vector2f Point44=drawNode(x44,y44,z44,window,w);
         
         //LUEGO UNIR NODOS CON ARISTAS
-        //drawEdge(window); //De nodo a nodo
+        drawEdge(Point1, Point2, window, w); //De nodo a nodo
+        drawEdge(Point2, Point3, window, w);
+        drawEdge(Point3, Point4, window, w);
+        drawEdge(Point4, Point1, window, w);
+
+        drawEdge(Point11, Point22, window, w);
+        drawEdge(Point22, Point33, window, w);
+        drawEdge(Point33, Point44, window, w);
+        drawEdge(Point44, Point11, window, w);
+
+        drawEdge(Point11, Point1, window, w); //De nodo a nodo
+        drawEdge(Point22, Point2, window, w);
+        drawEdge(Point33, Point3, window, w);
+        drawEdge(Point44, Point4, window, w);
+    }
+
+    void drawingCircle(sf::RenderWindow& window, World& w){
+        /*TEORIA FUNCION DE UN CIRCULO EN 3 EJES XYZ=
+         * se parametrizan las coordenadas x,y,z en funcion de un
+         * angulo theta (0<theta<2pi) definido por un centro (x0,y0,z0),
+         * radio r y vectores normales a su plano.
+         * x(tetha)=x0 + r*cos(theta)*ux + r*sin(tetha)*vx
+         * y(tetha)=y0 + r*cos(theta)*uy + r*sin(tetha)*vy
+         * z(tetha)=z0 + r*cos(theta)*uz + r*sin(tetha)*vz
+        */
+    }
+
+    void drawMotor(sf::RenderWindow& window, World& w){ 
+        drawingBox(window, w);
+        drawingCircle(window, w);
         //drawFunction(window); //para el dibujo practico
         //rotateAxeX(window);
         //rotateAxeY(window);
@@ -66,7 +122,7 @@ void execute(){
         }), "MOTOR 3D" 
     };
 
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(30);
 
     sf::Font font;
     if(!font.openFromFile("arial.ttf")) std::cerr<<"No se pudo cargar el archivo\n";
